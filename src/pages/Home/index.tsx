b30ed84e792ee71b2788b0263cd3dfa272b77d08
Jennifer Proust <jennifer.proust@gmail.com>
2020-11-25 18:10:35 +0100

    feat(home): load 500 stories and 50 details
    Needs a lot of improvements, see TODO.md

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,45 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@material-ui/core/styles';
-import { Box, Container, LinearProgress, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  LinearProgress,
+  Typography,
+} from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
-import {requestStories} from '../../state/actions/storiesActions';
-import { StoriesReducerType } from '../../state/reducers/storiesReducer';
+import {
+  requestStories,
+  requestStory,
+} from '../../state/actions/storiesActions';
+import {
+  StoriesReducerType,
+  StoryProps,
+} from '../../state/reducers/storiesReducer';
 import { RootReducersType } from '../../state/reducers';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    paddingBottom: theme.spacing(12),
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      paddingBottom: theme.spacing(12),
+    },
+    card: {
+      minWidth: 275,
+      marginBottom: theme.typography.pxToRem(12),
+    },
+    type: {
+      fontSize: theme.typography.pxToRem(14),
+    },
+    score: {
+      marginBottom: theme.typography.pxToRem(12),
+    },
+  }),
+);
 
 export const Home = () => {
   const { t } = useTranslation();
@@ -21,8 +48,20 @@ export const Home = () => {
   const {
     items: stories,
     loading: isLoadingStories,
+    loaded: isLoadedStories,
   }: StoriesReducerType = useSelector(
     ({ stories }: RootReducersType) => stories,
+  );
+  // const [extractStories, setExtractStories] = useState<StoryProps[]>([]);
+
+  const allDetailsLoaded = useSelector(
+    ({ stories: { items } }: RootReducersType) =>
+      items.every(story => story.loaded),
+  );
+
+  const isLoadingDetails = useSelector(
+    ({ stories: { items } }: RootReducersType) =>
+      items.some(story => story.loading),
   );
 
   const renderLoading = () => (
@@ -34,21 +73,81 @@ export const Home = () => {
     </>
   );
 
+  // TODO: create a component in another file
+  const renderStory = ({
+    id,
+    title,
+    url,
+    text,
+    loading,
+    type,
+    score,
+    loaded,
+  }: StoryProps) => {
+    console.log('RENDER STORY');
+    return (
+      <Card className={classes.card} key={id}>
+        <CardContent>
+          <Typography
+            className={classes.type}
+            color="textSecondary"
+            gutterBottom
+          >
+            {type}
+          </Typography>
+          <Typography variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography className={classes.score} color="textSecondary">
+            {score}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {text}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">
+            <a rel="noreferrer" href={url} target="_blank">
+              Visit
+            </a>
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  };
+
+  const fetchDetails = () => {
+    // TODO: create a pagination system
+    const extract = [...stories];
+    extract.length = 10;
+    Promise.all(extract.map(story => dispatch(requestStory(story.id))));
+  };
+
+  // When component is mounter, load the stories
   useEffect(() => {
     dispatch(requestStories());
   }, [dispatch]);
 
+  // When all the stories are loaded, load details for each
+  useEffect(() => {
+    isLoadedStories && fetchDetails();
+  }, [isLoadedStories]);
+
   return (
     <>
-      <Typography component="h1" variant="h1">
-        {t('home-title')}
-      </Typography>
       <Container maxWidth="sm" className={classes.root}>
+        <Typography component="h1" variant="h3">
+          {t('home-title')}
+        </Typography>
         <Box>
           {isLoadingStories ? (
             renderLoading()
           ) : (
-            <div>{stories.length > 0 && stories.join(' ')}</div>
+            <div>
+              {isLoadingDetails && <p>Loading details for each stories...</p>}
+              {allDetailsLoaded && <p>All details are loaded</p>}
+              {stories.length > 0 && stories.map(renderStory)}
+            </div>
           )}
         </Box>
       </Container>
--- src/state/reducers/storiesReducer.ts
+++ src/state/reducers/storiesReducer.ts
@@ -1,53 +1,89 @@
-import {storiesActionTypes as Actions, SUFFIX_FAILURE, SUFFIX_SUCCESS} from '../actionsTypes';
+import {
+  storiesActionTypes as Actions,
+  SUFFIX_FAILURE,
+  SUFFIX_SUCCESS,
+} from '../actionsTypes';
 
-// export type StoryProps = {
-//   id: number;
-//   deleted?: boolean; //	true if the item is deleted.
-//   type?: 'job' | 'story' | 'comment' | 'poll' | 'pollopt'; //	The type of item.
-//   by?: string; //	The username of the item's author.
-//   time?: number; //	Creation date of the item, in Unix Time.
-//   text?: string; //	The comment, story or poll text. HTML.
-//   dead?: boolean; //	true if the item is dead.
-//   parent?: number; //	The comment's parent: either another comment or the relevant story.
-//   poll?: number; //	The pollopt's associated poll.
-//   kids?: number[]; //	The ids of the item's comments, in ranked display order.
-//   url?: string; //	The URL of the story.
-//   score?: number; //	The story's score, or the votes for a pollopt.
-//   title?: string; //	The title of the story, poll or job. HTML.
-//   parts?: number[]; //	A list of related pollopts, in display order.
-//   descendants?: number; //	In the case of stories or polls, the total comment count.
-// };
+export type StoryProps = {
+  id: number;
+  deleted?: boolean; //	true if the item is deleted.
+  type?: 'job' | 'story' | 'comment' | 'poll' | 'pollopt'; //	The type of item.
+  by?: string; //	The username of the item's author.
+  time?: number; //	Creation date of the item, in Unix Time.
+  text?: string; //	The comment, story or poll text. HTML.
+  dead?: boolean; //	true if the item is dead.
+  parent?: number; //	The comment's parent: either another comment or the relevant story.
+  poll?: number; //	The pollopt's associated poll.
+  kids?: number[]; //	The ids of the item's comments, in ranked display order.
+  url?: string; //	The URL of the story.
+  score?: number; //	The story's score, or the votes for a pollopt.
+  title?: string; //	The title of the story, poll or job. HTML.
+  parts?: number[]; //	A list of related pollopts, in display order.
+  descendants?: number; //	In the case of stories or polls, the total comment count.
+  loading?: boolean;
+  loaded?: boolean;
+};
 
 export type StoriesReducerType = {
-  items: number[];
+  items: StoryProps[];
   loading: boolean;
+  loaded: boolean;
 };
 
 const initialState: StoriesReducerType = {
   items: [],
   loading: false,
+  loaded: false,
 };
 
 const storiesReducer = (
   state = initialState,
-  action: { type: Actions; payload: {data?: unknown} },
+  action: { type: Actions; payload: { data?: unknown } },
 ) => {
   switch (action.type) {
     case Actions.REQUEST_STORIES:
       return {
         ...state,
         loading: true,
+        loaded: false,
       };
     case `${Actions.REQUEST_STORIES}${SUFFIX_FAILURE}`:
       return {
         ...state,
         loading: false,
+        loaded: false,
       };
     case `${Actions.REQUEST_STORIES}${SUFFIX_SUCCESS}`:
       return {
         ...state,
-        items: action?.payload?.data as number[],
+        items: (action?.payload?.data as number[]).map(id => ({
+          id,
+          loaded: false,
+        })) as StoryProps[],
         loading: false,
+        loaded: true,
+      };
+    case Actions.REQUEST_STORY:
+      return {
+        ...state,
+      };
+    case `${Actions.REQUEST_STORY}${SUFFIX_FAILURE}`:
+      return {
+        ...state,
+      };
+    case `${Actions.REQUEST_STORY}${SUFFIX_SUCCESS}`:
+      console.log(action?.payload?.data);
+      return {
+        ...state,
+        items: state.items.map(item => {
+          const loadedItem = action.payload.data as StoryProps;
+          return item.id === loadedItem.id
+            ? {
+                ...item,
+                ...loadedItem,
+              }
+            : item;
+        }),
       };
     default:
       return state;
